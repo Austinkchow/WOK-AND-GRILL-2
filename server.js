043d6981106906b9c0f984f78a04f4b9c3bd4621
AustinkChow <austinkchow@gmail.com>
2020-05-29 05:58:30 -0700

    add routes, middleware, app configuration

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
+/* External Module */
 const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+/* Internal Modules */
+controller = require('./controllers');
+
+/* Instance Module */
 const app = express();
+
+/* Global Variables */
 const PORT = 4000;
 
+/* app configuration */
+app.use('view engine', 'ejs');
 
+/* Middleware */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
+/* Routes */
 
+//Root Route
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
+//Menu Route
+app.use('/menu', controllers.menu);
 
+//How to direct a item route
+
+//Binding Server to Port
 app.listen(PORT, () => {
-    console.log(`Listening http://localhost:${PORT}`);
-})
+  console.log(`Listening http://localhost:${PORT}`);
+});
