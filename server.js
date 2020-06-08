70f937eb6fec0a57f895e6f3e4259b8cee7c1d7a
AustinkChow <austinkchow@gmail.com>
2020-06-08 16:22:39 -0700

    changde nodemon to node

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-
+require('dotenv').config();
 /* Internal Modules */
 const controllers = require('./controllers');
 const adminRequired = require('./middleware/adminRequire');
@@ -13,7 +13,7 @@ const adminRequired = require('./middleware/adminRequire');
 const app = express();
 
 /* Global Variables */
-app.listen(process.env.PORT || 4000)
+const PORT = process.env.PORT;
 
 /* app configuration */
 app.set('view engine', 'ejs');
@@ -33,7 +33,7 @@ app.use(
     store: new MongoStore({
       url: process.env.MONGODB_URI || 'mongodb://localhost:27017/wokAndGrill',
     }),
-    secret: 'wok and grill',
+    secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: false,
     cookie: {
